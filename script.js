d2e1aeb481867ff7700cd4db233c99ab4846f022
Bianca <65398134+Binkybinx@users.noreply.github.com>
2024-11-14 16:32:37 +0200

    implement the logic

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click',function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  
+  // When no number is entered
+  if (!guess) {
+  document.querySelector('.message').textContent = 'No number';
+
+     // When guess is correct
+  } else if (guess === secretNumber) {
+     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+     //When guess is too high
+  }
+   else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
