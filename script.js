f7e8eb67b9b26021c04c987cec9c3481cfbdb4d4
Bianca <65398134+Binkybinx@users.noreply.github.com>
2024-11-14 17:39:35 +0200

    implement the highscore

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -16,8 +17,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //When guess is too high
   } else if (guess > secretNumber) {
@@ -47,7 +52,7 @@ document.querySelector('.again').addEventListener('click', function () {
 
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  
+
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
